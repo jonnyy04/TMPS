fade5245a2191200c0a0644367b8c06f68484017
jonnyy04 <151456268+jonnyy04@users.noreply.github.com>
2025-10-08 14:20:45 +0300

    folder structure

--- LAB_2/main/java/lab_2/Main.java
+++ LAB_2/main/java/lab_2/Main.java
@@ -1,7 +1,7 @@
-package oop.practice;
+package lab_2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hellllo world!");
     }
 }
